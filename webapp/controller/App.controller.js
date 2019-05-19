d5ce97a8a0d2d987a333dadf33ac65f6ce9d1645
marcelordrgs98@gmail.com <marcelordrgs98@gmail.com>
2019-05-19 17:50:02 +0000

    Adicionado formulário de busca

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,11 +1,16 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/base/Log"
+], function (Controller, Log) {
 	"use strict";
 
 	return Controller.extend("opensap.movies.controller.App", {
 		onInit: function () {
+			
+		},
 
+		onPress: function (oEvent) {
+			sap.m.MessageToast.show("Searching...");
 		}
 	});
 });
